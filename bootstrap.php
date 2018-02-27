1209b2b87ff6443238c0f2ffeaa9bae1a1c41282
DogSports <DogSports@users.noreply.github.com>
2018-02-27 22:40:19 +0100

    Update bootstrap.php
    For Flarum 0.1.0-beta7

--- bootstrap.php
+++ bootstrap.php
@@ -2,19 +2,19 @@
 
 /**
 * @package   dogsports\pipetables
-* @copyright Copyright (c) 2015-2017 The kevin PAULMIER Authors
+* @copyright Copyright (c) 2018 The kevin PAULMIER Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace dogsports\Flarum\PipeTables;
 
-use Flarum\Formatter\Event\Configuring;
+use Flarum\Event\ConfigureFormatter;
 use Illuminate\Events\Dispatcher;
 
 function subscribe(Dispatcher $events)
 {
 	$events->listen(
-		Configuring::class,
-		function (Configuring $event)
+		ConfigureFormatter::class,
+		function (ConfigureFormatter $event)
 		{
 			$event->configurator->PipeTables;
 		}
