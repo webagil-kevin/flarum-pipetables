beae4aeff991bad6a27e275242d394b3fce5d548
DogSports <DogSports@users.noreply.github.com>
2018-02-28 21:15:39 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,8 +4,8 @@
 * @copyright Copyright (c) 2018 The kevin PAULMIER Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-use DogSports\PipeTables\Listeners;
+namespace DogSports\PipeTables;
 use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
-    $events->subscribe(Listeners\AddAssets::class);
+    $events->subscribe(Listener\AddClientAssets::class);
 };
