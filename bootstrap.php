f70f10ca89563895886286eba46ec787eecd9569
DogSports <DogSports@users.noreply.github.com>
2018-02-28 20:12:48 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,24 +1,11 @@
 <?php
-
 /**
 * @package   dogsports\pipetables
 * @copyright Copyright (c) 2018 The kevin PAULMIER Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-namespace dogsports\Flarum\PipeTables;
-
-use Flarum\Event\ConfigureFormatter;
-use Illuminate\Events\Dispatcher;
-
-function subscribe(Dispatcher $events)
-{
-	$events->listen(
-		ConfigureFormatter::class,
-		function (ConfigureFormatter $event)
-		{
-			$event->configurator->PipeTables;
-		}
-	);
+use DogSports\PipeTables\Listeners;
+use Illuminate\Contracts\Events\Dispatcher;
+return function (Dispatcher $events) {
+    $events->subscribe(Listeners\AddAssets::class);
 };
-
-return __NAMESPACE__ . '\\subscribe';
