63806f333a41df6330825089ce6fd9ccea908185
DogSports <DogSports@users.noreply.github.com>
2018-03-05 20:42:02 +0100

    Code cleanup

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,28 +1,21 @@
-/* global $ */
-/* global app */
 import { extend } from 'flarum/extend'
 import Post from 'flarum/components/Post'
 
 app.initializers.add('DogSports-PipeTables', app => {
 	extend(Post.prototype, 'config', function (original, isInitialized) {
-		function addDataLabelsToTable(table)
-		{
+		function addDataLabelsToTable(table) {
 			let labels = [];
-			for (let th of table.getElementsByTagName('th'))
-			{
+			for (let th of table.getElementsByTagName('th')) {
 				labels.push(th.textContent);
 			}
-			for (let tr of table.getElementsByTagName('tr'))
-			{
+			for (let tr of table.getElementsByTagName('tr')) {
 				let i = -1;
-				for (let td of tr.getElementsByTagName('td'))
-				{
+				for (let td of tr.getElementsByTagName('td')) {
 					td.setAttribute('data-label', labels[++i] || '');
 				}
 			}
 		}
-		for (let table of document.getElementsByTagName('table'))
-		{
+		for (let table of document.getElementsByTagName('table')) {
 			addDataLabelsToTable(table);
 		}
 	})
