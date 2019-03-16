793d378bfbcff62382fe0511cdac17c3e6dd5380
KevID <9626689+KevID@users.noreply.github.com>
2019-03-16 17:37:45 +0100

    Update compatibility to flarum v0.1.0-beta.8.1

--- extend.php
+++ extend.php
@@ -18,8 +18,6 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
-    
-    new Extend\Locales(__DIR__ . '/resources/locale'),
 
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
